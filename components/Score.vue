33fc291ed0313931d116f353d5e0ef8ca2260e37
muramasa <muramasa125@gmail.com>
2019-07-24 23:54:04 +0900

    add Score.vue

--- components/Score.vue
+++ components/Score.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    <b-table></b-table>
+    <b-container>
+      <b-row>
+        <b-col class="col-1 float-left" v-for="set in sets" :key="set.name">
+          <div>{{ set.name }}</div>
+          <div>{{ set.a }}</div>
+          <div>{{ set.b }}</div>
+        </b-col>
+        <b-col class="col-1 float-left">
+          <div>point</div>
+          <div>{{ point.a }}</div>
+          <div>{{ point.b }}</div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 <script>
 export default {
+  data: function () {
+    return {
+      sets: [
+        { name: 'SET1', a: 1, b: 6 },
+        { name: 'SET2', a: 6, b: 3 },
+        { name: 'SET3', a: 7, b: 5 }
+      ],
+      point: { a: 40, b: 15 }
+    }
   }
+}
 </script>
 <style scoped>
 </style>
