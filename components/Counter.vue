db5668e1252ee2e8506c1311caf0fa5e8b834848
muramasa <muramasa125@gmail.com>
2019-07-24 23:54:29 +0900

    add Counter.vue

---
+++ components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col class="col-1 float-left">
+          <div>{{ set.name }}</div>
+          <div>{{ set.a }}</div>
+          <div>{{ set.b }}</div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      point: true
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
