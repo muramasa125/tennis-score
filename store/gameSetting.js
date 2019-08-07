358673ad7e3d9eb470ca47f303d8a3252b453aa1
Masashi Muramatsu <masashi_muramatsu@PC0038.local>
2019-08-07 13:02:06 +0900

    add vuex

---
+++ store/gameSetting.js
@@ -0,0 +1,19 @@
+export const state = {
+  gameSetting: {
+    set: null,
+    game: null,
+    ad: null
+  }
+}
+export const mutations = {
+  setGameSetting(state, set, game, ad) {
+    state.game.set = set
+    state.game.game = game
+    state.game.ad = ad
+  }
+}
+export const actions = {
+  setGameSetting({ commit }, { set, game, ad}) {
+    commit('setGameSetting', set, game, ad)
+  }
+}
